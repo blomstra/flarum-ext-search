8576579aa6fc4654ec975eb748b083e74e7fdbd5
Daniel Klabbers <daniel+git@klabbers.email>
2021-11-10 13:55:20 +0100

    use correct path on index

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -9,7 +9,7 @@ class Client extends \Flarum\Api\Client
 {
     public function get(string $path): ResponseInterface
     {
-        if ($path === '/discussions' && Arr::has($this->queryParams, 'filter.q')) return parent::get("/blomstra$path");
+        if ($path === '/discussions' && Arr::has($this->queryParams, 'filter.q')) return parent::get("/blomstra/search/discussions");
 
         return parent::get($path);
     }
