f21dc04a00027e890b233f6dbd9c48323a308f5f
Daniel Klabbers <daniel+git@klabbers.email>
2021-12-16 15:02:18 +0100

    attempt to make partial match filter

--- src/Api/Controllers/SearchController.php
+++ src/Api/Controllers/SearchController.php
@@ -57,9 +57,9 @@ class SearchController extends ListDiscussionsController
 
         if (! empty($search)) {
             $filterQuery
-                ->add($this->sentenceMatch($search))
-                ->add($this->wordMatch($search))
-                ->add($this->partialMatch($search));
+                ->add($this->sentenceMatch($search), 'should')
+                ->add($this->wordMatch($search), 'should')
+                ->add($this->partialMatch($search), 'should');
         }
 
         $builder = (new Builder($this->elastic))
